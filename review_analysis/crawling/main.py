bf284aa2506dddd4c17d53e7deae4deba7515431
CHOJaeGwan <jjgs0127@gmail.com>
2025-07-23 16:13:04 +0900

    임시 저장

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -1,11 +1,11 @@
 from argparse import ArgumentParser
 from typing import Dict, Type
-from review_analysis.crawling.base_crawler import BaseCrawler
-from review_analysis.crawling.example_crawler import ExampleCrawler
+from base_crawler import BaseCrawler
+from catchtable import CatchTableCrawler
 
 # 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
-    "example": ExampleCrawler,
+    "catchtable": CatchTableCrawler,
 }
 
 def create_parser() -> ArgumentParser:
