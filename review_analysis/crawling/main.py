19d5da52cb14ab44783eba28f3d56898cbf576ae
haseyKim <hshs6641@yonsei.ac.kr>
2025-07-24 23:26:09 +0900

    Update main.py
    change

--- review_analysis/crawling/main.py
+++ review_analysis/crawling/main.py
@@ -1,8 +1,7 @@
 from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.crawling.base_crawler import BaseCrawler
-from review_analysis.crawling.example_crawler import ExampleCrawler
-from review_analysis.crawling.kakaomap_crawler3 import KakaoCrawler
+from review_analysis.crawling.kakaomap_crawler import KakaoCrawler
 
 # 모든 크롤링 클래스를 예시 형식으로 적어주세요. 
 CRAWLER_CLASSES: Dict[str, Type[BaseCrawler]] = {
